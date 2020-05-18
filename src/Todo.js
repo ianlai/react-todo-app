487d3627e560f671222b4f4d606d1ed439902199
Ian <ian.explore.world@gmail.com>
2020-05-18 19:07:42 +0900

    Removed done button; gray out when todo is done
    1. Removed done button
    2. Made the whole li clickable (toggle status)
    3. Made gray out when the todo item is done
    4. Changed to css flexbox to handle the layout of li

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+
+
 class Todo extends Component{
     constructor(props){
         super(props)
@@ -18,16 +20,12 @@ class Todo extends Component{
     render(){
         const {id, name, isDone} = this.props;
         return (
-            <div className = "todo-item">
-                <li className="list-group-item"> 
-                    <span className ="todo-item">
-                        [{id}] {name} ----- {isDone ? "O" : "X"}
-                    </span>
-                    <button className = "todo-button" onClick={this.setDone}>Done</button> 
-                    <button className = "todo-button" onClick={this.remove}>Delete</button>     
-                </li>
-                
-            </div>
+            <li className="list-group-item"> 
+                <span className = {isDone ? "todo-done" : ""} onClick={this.setDone}>
+                    [{id}] {name}
+                </span>
+                <button className = "todo-button" onClick={this.remove}>Delete</button>     
+            </li>
         )
     }
 }
